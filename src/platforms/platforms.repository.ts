75f9bedb065e86cce5db49ab5a7e60a2068693db
Alex <djaday20161@gmail.com>
2022-08-30 22:55:19 +0500

    bug fixes

--- src/platforms/platforms.repository.ts
+++ src/platforms/platforms.repository.ts
@@ -7,13 +7,14 @@ import { IPlatformRepository } from './platforms.repository.interface';
 export class PlatformsRepository implements IPlatformRepository {
 
 	async getAll(): Promise<PlatformDescription[]> {
-		const platforms = await import('./data/platforms.json');
+		const platforms = await import('../../data/platforms.json');
 		return platforms.default;
 	}
 
 	async getPlatformById (id: string): Promise<Platform>
 	{
-		const platform = await import(`./data/${id}.json`);
+		const platform = await import(`../../../data/${id}.json`);
+		console.log();
 		return platform.default;
 	}
 }
